71dbf46733561aded28d27c819aa054611bc70ba
Shreyas Kulkarni <kshreyas93241@gmail.com>
2025-01-16 14:57:46 +0530

    File in class

---
+++ FileIOSess/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileIOSess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileIO.ReadText();
+
+            //FileIO.WriteText();
+
+            //FileIO.AppendText();
+
+            //FileIO.CountWords();
+
+            FileIO.FindWord();
+        }
+    }
+}
